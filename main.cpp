e9de7b0161023ed5845242364d9de3807edfd6ed
Joseph Pecoraro <jpecoraro342@ufl.edu>
2014-01-18 20:09:38 -0500

    Hi/Low card game for testing

--- main.cpp
+++ main.cpp
@@ -7,18 +7,67 @@
 //
 
 #include <iostream>
+#include <string>
 #include "Deck.h"
+#include "Card.h"
+#include <ctype.h>
+
+bool playHiLo(Deck * cardDeck) {
+    char choice;
+    Card playercard = *cardDeck->draw();
+    playercard.print();
+    cout << "Hi or Low(H/L)?: ";
+    cin >> choice;
+    choice = toupper(choice);
+    Card nextCard = *cardDeck->draw();
+    nextCard.print();
+    if (nextCard.getValue()-playercard.getValue() >= 0) {
+        if (choice == 'H') {
+            cout << "Correct! the next card was higher" << endl;
+            return true;
+        }
+        else {
+            cout << "Sorry, the next card was lower" << endl;
+            return false;
+        }
+    }
+    else if (nextCard.getValue()-playercard.getValue() < 0) {
+        if (choice == 'L') {
+            cout << "Correct! the next card was lower" << endl;
+            return true;
+        }
+        else {
+            cout << "Sorry, the next card was higher" << endl;
+            return false;
+        }
+    }
+    return true;
+}
+
 
 int main(int argc, const char * argv[])
 {
     Deck cardDeck = *new Deck();
     cardDeck.fillDeck();
-    cardDeck.printDeck();
-    std::cout<< std::endl << std::endl;
     cardDeck.shuffle();
-    cardDeck.printDeck();
-    std::cout<< std::endl << std::endl;
-    cardDeck.shuffle();
-    cardDeck.printDeck();
+    
+    bool win = true;
+    char continu;
+    
+    while (win) {
+        win = playHiLo(&cardDeck);
+        if (!win) {
+            cout << "keep playing(y/n)?: ";
+            cin >> continu;
+            continu = toupper(continu);
+            
+            if (continu == 'Y') {
+                win = true;
+            }
+            else {
+                win=false;
+            }
+        }
+    }
 }
 
